c75c3552666cbc1adda2ad7ca799b686cbe597e1
maha <97425755+maha004dodla@users.noreply.github.com>
2023-06-30 18:31:55 +0530

    Added solution - LeetHub

---
+++ Is Binary Number Multiple of 3 - GFG/is-binary-number-multiple-of-3.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+//User function template for C++
+class Solution{
+public:	
+		
+	int isDivisible(string s)
+	{
+	    long long c=0;
+	    for(int i=0; i<s.size(); i++)
+	    {
+	        if(s[i]=='1')
+	        {
+	            if(i%2==0)
+	            {
+	                c++;
+	            }
+	            else
+	            {
+	                c--;
+	            }
+	        }
+	    }
+	    if(c%3==0)
+	    {
+	        return 1;
+	    }
+	    return 0;
+	}
+
+};
+
+//{ Driver Code Starts.
+int main(){
+    
+    int t;
+    cin >> t;
+    while(t--){
+        string s;
+        cin >> s;
+        Solution ob;
+        cout << ob.isDivisible(s) << endl;
+    }
+return 0;
+}
+
+
+// } Driver Code Ends
