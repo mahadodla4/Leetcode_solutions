a0fab31d94e88dad2dea0917672e02a89bfec4f7
maha <97425755+maha004dodla@users.noreply.github.com>
2023-08-01 09:44:40 +0530

    Time: 63 ms (99.94%), Space: 18.2 MB (67.82%) - LeetHub

--- 0077-combinations/0077-combinations.py
+++ 0077-combinations/0077-combinations.py
@@ -6,6 +6,7 @@ class Solution:
         for i in range(1,n+1):
             l.append(i)
         com=combinations(l,k)
-        for i in list(com):
+        com=list(com)
+        for i in com:
             m.append(i)
         return m
