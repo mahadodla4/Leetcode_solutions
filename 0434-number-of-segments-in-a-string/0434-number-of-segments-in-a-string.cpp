ad23a84ffa8517d9fbb54836edac337ea8c450b9
maha <97425755+maha004dodla@users.noreply.github.com>
2023-08-26 11:47:58 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (91.83%) - LeetHub

---
+++ 0434-number-of-segments-in-a-string/0434-number-of-segments-in-a-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int countSegments(string s) 
+    {
+        stringstream ss(s);
+        string word;
+        int count=0;
+        while(ss >> word)
+        {
+            count++;
+        }
+        return count;
+    }
+};
