092bd60161d0d586e7bba4d3442251266f808d29
maha <97425755+maha004dodla@users.noreply.github.com>
2023-09-28 09:19:51 +0530

    Added solution - LeetHub

--- Wave Array - GFG/wave-array.cpp
+++ Wave Array - GFG/wave-array.cpp
@@ -10,22 +10,12 @@ class Solution{
     // arr: input array
     // n: size of array
     //Function to sort the array into a wave-like array.
-    
     void convertToWave(int n, vector<int>& arr)
     {
-        int i;
-        for(i=0; i<n-1; i++)
+        for(int i=0; i<n-1; i+=2)
         {
-            if(i%2==0 && arr[i]<arr[i+1])
-            {
-                swap(arr[i],arr[i+1]);
-            }
-            /*if(i%2!=0 && arr[i]>arr[i+1])
-            {
-                swap(arr[i],arr[i+1]);
-            }*/
+            swap(arr[i],arr[i+1]);
         }
-        
     }
 };
 
