8a961214bf67bf7c263b49686da404b0e8912405
maha <97425755+maha004dodla@users.noreply.github.com>
2023-11-24 20:46:27 +0530

    Added solution - LeetHub

---
+++ Diagonal sum - GFG/diagonal-sum.py
@@ -0,0 +1,28 @@
+#User function Template for python3
+
+class Solution:
+	def DiagonalSum(self, matrix):
+		# Code here
+		sum=0
+		n=len(matrix)
+		for i in range(n):
+		      sum+=matrix[i][i]
+		      sum+=matrix[i][n-1-i];
+		return sum
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+if __name__ == '__main__':
+	T=int(input())
+	for i in range(T):
+		n = int(input())
+		matrix = []
+		for _ in range(n):
+			matrix.append(list(map(int,input().split())))
+		ob = Solution()
+		ans = ob.DiagonalSum(matrix)
+		print(ans)
+# } Driver Code Ends
