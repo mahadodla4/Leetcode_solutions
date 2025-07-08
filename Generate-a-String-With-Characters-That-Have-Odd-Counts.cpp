1f9c78d229f2c422ccda01fd8d2c5d5ab1a30933
Mahalakshmi Dodla <97425755+mahadodla4@users.noreply.github.com>
2025-07-08 12:10:27 +0530

    [1374] [Time Beats: 100.00%] [Memory Beats: 92.37%] - LeetPush

---
+++ Generate-a-String-With-Characters-That-Have-Odd-Counts.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    string generateTheString(int n) {
+        string str;
+        if(n%2!=0) {
+          for(int i=1; i<=n; i++) {
+            str+='a';
+          }
+        } else {
+            for(int i=1; i<n; i++) {
+                str+='a';
+            } 
+            str+='b';
+        }
+        return str;
+    }
+};
